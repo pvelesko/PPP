13b8db54686429eb614cf3c1ddf373b1df53ec28
Paulius Velesko <paulius.velesko@intel.com>
2020-04-16 13:45:40 +0000

    update template

--- languages/c++/sycl/template/main.cpp
+++ languages/c++/sycl/template/main.cpp
@@ -1,31 +1,10 @@
 #include <CL/sycl.hpp>
-//#include "Util.hpp"
+#include "SyclUtil.hpp"
 #include <stdlib.h>
 #include <iostream>
-#include <complex>
 using namespace cl::sycl;
 int main(int argc, char** argv) {
-  //process_args(argc, argv);
-  //init();
-  int n = 3;
-  auto q  = queue(gpu_selector{});
-
-  std::complex<float>* t = (std::complex<float>*)malloc_shared(n * sizeof(std::complex<float>), q); 
-  auto nn = n;
-  q.submit([&] (handler& cgh) { // q scope
-    cgh.parallel_for<class oneplusone>(range<1>(n), [=](id<1> i) {
-      for (int j = 0; j < nn; j++)
-        t[i] = t[0] * t[j];
-    }); // end task scope
-  }); // end q scope
-  q.wait();
-  std::cout << t[0] << std::endl;
-
-#pragma omp target map(tofrom:t[0:3])
-  {
-    for (int j = 0; j < nn; j++)
-      t[0] = t[0] * t[j];
-  }
-
+  process_args(argc, argv);
+  init();
   return 0;
 }
