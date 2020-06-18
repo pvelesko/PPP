abee1e5436c898064930f2b1f69ab94c31685d21
Paulius Velesko <paulius.velesko@intel.com>
2020-06-18 15:47:22 +0000

    openptarget vs sycl

--- languages/c++/sycl/kokkos_sycl/main.cpp
+++ languages/c++/sycl/kokkos_sycl/main.cpp
@@ -1,29 +1,40 @@
-#include <CL/sycl.hpp>
+//#include <CL/sycl.hpp>
+#include "Kokkos_Core.hpp"
 #include <stdlib.h>
 #include <iostream>
-#include "Kokkos_Core.hpp"
-using namespace cl::sycl;
+//using namespace cl::sycl;
 
-using MemSpace = Kokkos::Experimental::SYCLHostUSMSpace;
-using ExSpace = Kokkos::Experimental::SYCL::execution_space;
+using HostSpace = Kokkos::HostSpace;
+
+//using MemSpace = Kokkos::Experimental::SYCLHostUSMSpace;
+//using ExSpace = Kokkos::Experimental::SYCL;
+using MemSpace = Kokkos::Experimental::OpenMPTargetSpace;
+using ExSpace = Kokkos::Experimental::OpenMPTarget;
+//using MemSpace = Kokkos::HostSpace;
+//using ExSpace = Kokkos::Serial;
 using KokkosDevice = Kokkos::Device<ExSpace, MemSpace>;
-using CPUSpace = Kokkos::HostSpace;
-using HostKokkosDevice = Kokkos::Device<CPUSpace, MemSpace>;
 
 int main(int, char**)
 {
+  int num[3];
   Kokkos::initialize();
   {
-  Kokkos::View<int*, KokkosDevice> num_v("label", 3);
+  //Kokkos::View<int*, KokkosDevice> num_v("label", 3);
+  Kokkos::View<int*, KokkosDevice> num_v(num, 3);
+  Kokkos::View<int*, HostSpace> num_h(num, 3);
   num_v(0) = 1; num_v(1) = 1; num_v(2) = 0;
   auto lam = [=](const int idx) {
-    num_v(2) = num_v(idx) + sqrt(num_v(idx));
+    //num_v(2) = num_v(idx) + (num_v(idx));
+    num_v(2) = 99;
   };
 
   Kokkos::parallel_for(2, lam);
+  Kokkos::fence();
   std::cout << "1 + 1 = " << num_v(2) << std::endl;
+  Kokkos::deep_copy(num_h, num_v);
   }
   Kokkos::finalize();
+  std::cout << "1 + 1 = " << num[2] << std::endl;
   return 0;
 }
 
